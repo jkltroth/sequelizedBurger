acd069bf6ca65d35c4378cda3ab0a69437877626
James Roth <jkltroth@gmail.com>
2018-01-15 20:44:03 -0500

    modified model file to reflect sequelize syntax

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,17 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-
-var burger = {
-    selectAll: function (callback) {
-        orm.selectAll("burgers", function (res) {
-            callback(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    insertOne: function (cols, vals, callback) {
-        orm.insertOne("burgers", cols, vals, function (res) {
-            callback(res);
-        });
-    },
-    updateOne: function (objColVals, condition, callback) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
-            callback(res);
-        });
-    }
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("burgers", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+    return Burger;
 };
-
-// Export the database functions for the controller (burger_controller.js).
-module.exports = burger;
